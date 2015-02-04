abfb1882b2a68e8135e814c0ac09a6c1f3eb2f52
Karl Pietri <karl@threadbaregames.com>
2015-02-04 22:10:39 +0000

    jmeter shouldn't manage zip

--- manifests/init.pp
+++ manifests/init.pp
@@ -23,10 +23,6 @@ class jmeter(
     ensure => present,
   }
 
-  package { 'unzip':
-    ensure => present,
-  }
-
   exec { 'download-jmeter':
     command => "wget -P /root http://archive.apache.org/dist/jmeter/binaries/apache-jmeter-${jmeter_version}.tgz",
     creates => "/root/apache-jmeter-${jmeter_version}.tgz"
